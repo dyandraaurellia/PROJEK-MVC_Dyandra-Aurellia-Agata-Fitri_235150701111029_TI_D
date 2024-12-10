43ba6efda42806f878ade4fe0422e840da6b9f04
Dyandra Aurellia Agata Fitri <160992091+dyandraaurellia@users.noreply.github.com>
2024-12-10 22:02:27 +0700

    MovieController.php

--- controllers/MovieController.php
+++ controllers/MovieController.php
@@ -3,8 +3,8 @@ require_once 'models/Movie.php';
 
 class MovieController {
     public function index() {
-        $movie = Movie::getAll(); // Ambil semua data film
-        include 'views/home.php'; // Kirim data ke view
+        $movie = Movie::getAll(); 
+        include 'views/home.php';
     }
 
     public function detail($id) {
@@ -14,7 +14,6 @@ class MovieController {
 
     public function add() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // Ambil data dari form
             $title = $_POST['title'];
             $genre = $_POST['genre'];
             $director = $_POST['director'];
@@ -24,29 +23,24 @@ class MovieController {
     
             // Handle file upload
             $poster = $_FILES['poster']['name'];
-            $targetDir = "uploads/"; // Direktori tempat file di-upload
-            $targetFile = $targetDir . basename($poster); // Path lengkap untuk file yang di-upload
+            $targetDir = "uploads/"; 
+            $targetFile = $targetDir . basename($poster); 
     
-            // Pastikan direktori 'uploads/' ada, jika belum buat
             if (!is_dir($targetDir)) {
                 mkdir($targetDir, 0777, true);
             }
     
-            // Upload file ke direktori 'uploads/'
             if (move_uploaded_file($_FILES['poster']['tmp_name'], $targetFile)) {
                 echo "File successfully uploaded.";
             } else {
                 echo "File upload failed.";
             }
     
-            // Panggil method untuk menyimpan data film ke database
             Movie::create($title, $genre, $director, $releaseDate, $duration, $synopsis, $poster);
     
-            // Redirect ke halaman utama setelah berhasil menambahkan film
             header('Location: index.php?action=home');
             exit();
         } else {
-            // Tampilkan form untuk menambahkan film
             require_once 'views/addMovie.php';
         }
     }
@@ -82,7 +76,7 @@ class MovieController {
 
     public function delete($id) {
         Movie::delete($id);
-        header('Location: index.php?action=home'); // Arahkan ke halaman daftar film setelah delete
+        header('Location: index.php?action=home'); 
         exit();
     }    
 }
